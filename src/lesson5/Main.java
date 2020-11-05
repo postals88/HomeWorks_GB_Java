7c96e7aaa3947b09ab2f731a69470f9a0eb617a3
Dmitry <Dumanrus@mail.ru>
2020-11-05 21:06:08 +0300

    прошла лекция 5

--- src/lesson5/Main.java
+++ src/lesson5/Main.java
@@ -1,2 +1,7 @@
-package lesson5;public class Main {
+package lesson5;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Просмотрел и законспектировал лекцию!");
+    }
 }
