45d3f6524c4247bf2342e008aae3e77baa6465b4
Dmitry <Dumanrus@mail.ru>
2020-11-11 21:52:41 +0300

    Лекция7

--- src/lesson7/Main.java
+++ src/lesson7/Main.java
@@ -1,2 +1,7 @@
-package lesson7;public class Main {
+package lesson7;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Просмоотрел лекцию,законспектировал");
+    }
 }
