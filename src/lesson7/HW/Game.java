4097c75fd06716192ff73623e5cee8db80664c47
Dmitry <Dumanrus@mail.ru>
2020-11-13 23:22:46 +0300

    HW7

--- src/lesson7/HW/Game.java
+++ src/lesson7/HW/Game.java
@@ -1,2 +1,7 @@
-package lesson7.HW;public class Game {
+package lesson7.HW;
+
+public class Game {
+    public static void main(String[] args) {
+        new MainWindow(); //Показывает наше окно игры
+    }
 }
--- src/lesson7/HW/GameMap.java
+++ src/lesson7/HW/GameMap.java
@@ -1,2 +1,23 @@
-package lesson7.HW;public class GameMap {
+package lesson7.HW;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GameMap extends JPanel { // почему наследуем Jpanel????????
+    public static final int MODE_HVA = 0;
+    public static final int MODE_HVH = 1;
+
+    GameMap() {
+        setBackground(Color.black); // указываем цвет карты поля
+
+    }
+
+    void startNewGame(int mode, int fieldSizeX, int fieldSizeY, int winLength) { //режим игры, размеры поля, количество игроков, победная серия
+        // метод для того чтобы понять что должен знать класс карты,для того что бы начать игру
+        System.out.println("Режим игры = " + mode +
+                "\n Размер поля = " + fieldSizeX +
+                "\n Количество игрово = " + fieldSizeY +
+                "\n Победная серия = " + winLength);
+
+    }
 }
